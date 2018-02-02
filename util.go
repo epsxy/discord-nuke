5d60be114405b27a4779a36d9d79da7b2e619d49
lamados <lamados@yandex.com>
2018-02-02 14:35:25 +0000

    forgot to change import name from dgo to discordgo also changed comment slightly

--- util.go
+++ util.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 
-	dgo "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 func percent(a, b float64) float64 {
@@ -20,20 +20,19 @@ func equal(a, b float64) bool {
 }
 
 // these functions are used to quickly get structs from their IDs
-// DO NOT USE if you're not 100% certain that an ID is correct
-// i'm only using these because i'm a terrible programmer
+// i'm only using these because i'm a terrible lazy programmer
 
-func guild(guildID string) *dgo.Guild {
+func guild(guildID string) *discordgo.Guild {
 	g, _ := session.Guild(guildID)
 	return g
 }
 
-func channel(channelID string) *dgo.Channel {
+func channel(channelID string) *discordgo.Channel {
 	c, _ := session.Channel(channelID)
 	return c
 }
 
-func user(userID string) *dgo.User {
+func user(userID string) *discordgo.User {
 	u, _ := session.User(userID)
 	return u
 }
